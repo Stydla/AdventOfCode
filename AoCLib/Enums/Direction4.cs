5d2c8019ebdad299499b35697fc51828dfe6af0b
Ondra <Ondra@ONDRA-PC>
2023-01-08 18:40:10 +0100

    Enums

---
+++ AoCLib/Enums/Direction4.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoCLib.Enums
+{
+  public enum EDirection4
+  {
+    UP = 0,
+    RIGHT = 1,
+    DOWN = 2,
+    LEFT = 3
+  }
+}
---
+++ AoCLib/Enums/Direction8.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoCLib.Enums
+{
+  public enum EDirection8
+  {
+    UP = 0,
+    UP_RIGHT = 1,
+    RIGHT = 2,
+    DOWN_RIGHT = 3,
+    DOWN = 4,
+    DOWN_LEFT = 5,
+    LEFT = 6,
+    UP_LEFT = 7
+  }
+}
