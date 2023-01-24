3bdabd3744cbca9ceaca56abaa5cf13ad7770394
Ondřej Brožek <obrozek@centrum.cz>
2023-01-24 18:13:43 +0100

    17

---
+++ AoCLib/MD5Creator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoCLib
+{
+  public class MD5Creator
+  {
+    public static string Create(string input)
+    {
+      using(MD5 MD5 = MD5.Create())
+      {
+        byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+        byte[] hashBytes = MD5.ComputeHash(inputBytes);
+
+        StringBuilder sb = new System.Text.StringBuilder();
+        for (int i = 0; i < hashBytes.Length; i++)
+        {
+          sb.Append(hashBytes[i].ToString("X2"));
+        }
+        return sb.ToString().ToLower();
+      }
+    }
+  }
+}
