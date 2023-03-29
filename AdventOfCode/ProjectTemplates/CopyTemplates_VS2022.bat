e991c721051d75f8e83539d6e69e630c17a5287b
Ondřej Brožek <obrozek@centrum.cz>
2023-03-29 11:28:40 +0200

    copyTemplate fix

--- AdventOfCode/ProjectTemplates/CopyTemplates_VS2022.bat
+++ AdventOfCode/ProjectTemplates/CopyTemplates_VS2022.bat
@@ -9,3 +9,15 @@ copy AdventSolver2018.zip "%userprofile%\documents\Visual Studio 2022\Templates\
 copy AdventSolver2017.zip "%userprofile%\documents\Visual Studio 2022\Templates\ProjectTemplates\AdventSolver2017.zip"
 copy AdventSolver2016.zip "%userprofile%\documents\Visual Studio 2022\Templates\ProjectTemplates\AdventSolver2016.zip"
 copy AdventSolver2015.zip "%userprofile%\documents\Visual Studio 2022\Templates\ProjectTemplates\AdventSolver2015.zip"
+
+tar -xf "%userprofile%\documents\Visual Studio 2022\Templates\ProjectTemplates\AdventSolver2025.zip" -C "%userprofile%\documents\Visual Studio 2022\Templates\ProjectTemplates
+tar -xf "%userprofile%\documents\Visual Studio 2022\Templates\ProjectTemplates\AdventSolver2024.zip" -C "%userprofile%\documents\Visual Studio 2022\Templates\ProjectTemplates
+tar -xf "%userprofile%\documents\Visual Studio 2022\Templates\ProjectTemplates\AdventSolver2023.zip" -C "%userprofile%\documents\Visual Studio 2022\Templates\ProjectTemplates
+tar -xf "%userprofile%\documents\Visual Studio 2022\Templates\ProjectTemplates\AdventSolver2022.zip" -C "%userprofile%\documents\Visual Studio 2022\Templates\ProjectTemplates
+tar -xf "%userprofile%\documents\Visual Studio 2022\Templates\ProjectTemplates\AdventSolver2021.zip" -C "%userprofile%\documents\Visual Studio 2022\Templates\ProjectTemplates
+tar -xf "%userprofile%\documents\Visual Studio 2022\Templates\ProjectTemplates\AdventSolver2020.zip" -C "%userprofile%\documents\Visual Studio 2022\Templates\ProjectTemplates
+tar -xf "%userprofile%\documents\Visual Studio 2022\Templates\ProjectTemplates\AdventSolver2019.zip" -C "%userprofile%\documents\Visual Studio 2022\Templates\ProjectTemplates
+tar -xf "%userprofile%\documents\Visual Studio 2022\Templates\ProjectTemplates\AdventSolver2018.zip" -C "%userprofile%\documents\Visual Studio 2022\Templates\ProjectTemplates
+tar -xf "%userprofile%\documents\Visual Studio 2022\Templates\ProjectTemplates\AdventSolver2017.zip" -C "%userprofile%\documents\Visual Studio 2022\Templates\ProjectTemplates
+tar -xf "%userprofile%\documents\Visual Studio 2022\Templates\ProjectTemplates\AdventSolver2016.zip" -C "%userprofile%\documents\Visual Studio 2022\Templates\ProjectTemplates
+tar -xf "%userprofile%\documents\Visual Studio 2022\Templates\ProjectTemplates\AdventSolver2015.zip" -C "%userprofile%\documents\Visual Studio 2022\Templates\ProjectTemplates
