4838f05691841544cdae244b8914aa53b610a150
Ondřej Brožek <obrozek@centrum.cz>
2024-12-06 07:07:50 +0100

    6

--- AoCLib/Enums/Direction4.cs
+++ AoCLib/Enums/Direction4.cs
@@ -13,4 +13,48 @@ namespace AoCLib.Enums
     DOWN = 2,
     LEFT = 3
   }
+
+  public static class EDirection4Helper
+  {
+    public static EDirection4 Parse(char value)
+    {
+      switch (value)
+      {
+        case '>':
+          return EDirection4.RIGHT;
+        case 'v':
+          return EDirection4.DOWN;
+        case '<':
+          return EDirection4.LEFT;
+        case '^':
+          return EDirection4.UP;
+      }
+      throw new Exception($"Invalid EDirection4 value: {value}");
+    }
+    public static bool TryParse(char value, out EDirection4 direction) 
+    { 
+      if(value == '>')
+      {
+        direction = EDirection4.RIGHT;
+        return true;
+      }
+      if (value == 'v')
+      {
+        direction = EDirection4.DOWN;
+        return true;
+      }
+      if (value == '<')
+      {
+        direction = EDirection4.LEFT;
+        return true;
+      }
+      if (value == '^')
+      {
+        direction = EDirection4.UP;
+        return true;
+      }
+      direction = EDirection4.UP;
+      return false;
+    }
+  }
 }
